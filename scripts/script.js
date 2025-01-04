d18257c7fc85f3b94e3e3cfb396485665939f94a
yashdd <yashdeshpande877@gmail.com>
2025-01-04 09:49:46 +0530

    Added Hero About and Skills Section

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the navigation menu on smaller screens
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
